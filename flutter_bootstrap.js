418388b736d0838a94c77df8327be5874f7cac22
pranave2003 <pranav2003a10@gmail.com>
2024-12-31 11:18:39 +0530

    logo changed and scrollingwork comoleted

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3020661515"
+    serviceWorkerVersion: "3423119837"
   }
 });
